0cb3134b2e41bf1f3aaab68283164177674196a1
luowei <3245554@qq.com>
2019-01-25 15:46:37 +0800

    更新tomcat.sh

--- ghy-opvs/ghy-create_tomcat.sh
+++ ghy-opvs/ghy-create_tomcat.sh
@@ -22,23 +22,33 @@ echo "Create the new_object path and enter……"
 if [ ! -e $Total_directory ];then
     mkdir $Total_directory
     echo Enter $Total_directory directory
-    cd /opt/new_project
+    cd $Total_directory
 fi
+
+Extract_to_site(){
+    echo "Extract to the admin package site"
+    if [ ! -e apache-tomcat-8.5.37 ];then	
+       tar -xvf apache-tomcat-8.5.37.tar.gz
+    fi
+    cp -arf apache-tomcat-8.5.37 admin-package
+    rm -rf admin-package/webapps/*
+    sed -i 's/8080/8086/' admin-package/conf/server.xml
+    sh admin-package/bin/startup.sh
+    rm -rf apache-tomcat-8.5.37 #清除多余目录
+    echo "Extract to the chat package site"
+    cp -arf admin-package chat-package
+    sed -i 's/8086/8087/' chat-package/conf/server.xml
+    sh chat_package/bin/startup.sh
+    echo "Service startup result state"
+    echo -e "\033[31m `ps -aux|grep admin-package`\033[0m"
+    echo -e "\033[31m `ps -aux|grep chat-package`\033[0m"
+}
 
 echo " "
 if [ ! -f apache-tomcat-8.5.37.tar.gz ];then
     echo "Start downloading tomcat, version 8.5.37……"
     wget http:\/\/mirrors.hust.edu.cn\/apache\/tomcat\/tomcat\-8\/v8.5.37\/bin\/apache-tomcat-8.5.37.tar.gz
+    Extract_to_site
 else
-    tar -xvf apache-tomcat-8.5.37.tar.gz
-    echo "Extract to the admin package site"
-    mv -f apache-tomcat-8.5.37 admin-package
-    rm -rf admin-package/webapps/*
-    sh admin-package/bin/startup.sh
-    echo "Extract to the chat package site"
-    cp -arf admin-package chat_package
-    sed -i 's/8080/8087/' admin-package/conf/server.xml
-    sh chat_package/bin/startup.sh
-else
-    echo "The tomcat package download failed and the associated package files were not found"
+    Extract_to_site
 fi

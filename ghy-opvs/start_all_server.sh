49df2fe5b1c8bdb9293ee8b8c91e3055ea5c3db4
luowei <3245554@qq.com>
2019-01-25 17:59:02 +0800

    更新tomcat.sh

--- ghy-opvs/start_all_server.sh
+++ ghy-opvs/start_all_server.sh
@@ -22,15 +22,56 @@ make_nginx_file_folder=/usr/local/nginx/html
 declare -A r_port_table_array
 declare -A SID_table_array
 #Define an r_port array table
-r_port_table_array=([center]=50001 [db]=50002 [baccara]=50003 [bullfight]=50004 [classicLandords]=50005 [cqeverycolor]=50006 \
-[cqssc]=50007 [dragontiger]=50008 [fores]=50009 [fruit-machine]=50010 [gemstorm]=50011 [glodflower]=50012 [goodstart]=50013 \
-[goodstart]=50014 [gragontiles]=50015 [red-black]=50016 [robtaurus]=50017 [gate]=50018 [hall]=50019 [log]=50020 [login]=50021 \
-[download]=50022 [ip]=50023 [pay]=50024 [promotion]=50025)
+r_port_table_array=([center]=50001 \
+[db]=50002 \
+[baccara]=50003 \
+[bullfight]=50004 \
+[classicLandords]=50005 \
+[cqeverycolor]=50006 \
+[cqssc]=50007 \
+[dragontiger]=50008 \
+[fores]=50009 \
+[fruit-machine]=50010 \
+[gemstorm]=50011 \
+[glodflower]=50012 \
+[goodstart]=50013 \
+[texas-poker]=50014 \
+[gragontiles]=50015 \
+[red-black]=50016 \
+[robtaurus]=50017 \
+[gate]=50018 \
+[hall]=50019 \
+[log]=50020 \
+[login]=50021 \
+[download]=50022 \
+[ip]=50023 \
+[pay]=50024 \
+[promotion]=50025 \
+[turntaurus]=50026 )
 #Define an SID array table
-SID_table_array=([center]=20001 [db]=20002 [baccara]=20003 [bullfight]=20004 [classicLandords]=20005 [cqeverycolor]=20006 \
-[cqssc]=20007 [dragontiger]=20008 [fores]=20009 [fruit-machine]=20010 [gemstorm]=20011 [glodflower]=20012 [goodstart]=20013 \
-[goodstart]=20014 [gragontiles]=20015 [red-black]=20016 [robtaurus]=20017 [gate]=20018 [hall]=20019 [log]=20020 [login]=20021 \
-[download]=20022 [ip]=20023 [pay]=20024 [promotion]=20025)
+SID_table_array=([fores]=2106 \
+[bullfight]=2105 \
+[glodflower]=2101 \
+[red-black]=2102 \
+[robtaurus]=2112 \
+[classicLandords]=2100 \
+[baccara]=2103 \
+[texas-poker]=2104 \
+[gemstorm]=2113 \
+[dragontiger]=2108 \
+[turntaurus]=2109 \
+[fruit-machine]=2107 \
+[gragontiles]=2111 \
+[goodstart]=2110 \
+[cqssc]=2114 \
+[cqeverycolor]=2117 \
+[log]=6100 \
+[db]=8100 \
+[hall]=3100 \
+[login]=4100 \
+[center]=9100 \
+[gate]=1100 )
+
 
 #Get the git repository to get the latest update package
 Git_Repository_Pull_Method(){
@@ -245,27 +286,40 @@ Start_all(){
             echo "This application remote monitoring IP address:["$r_host"]"
             r_port=Get_soft_array_table_rport $soft_directory
             echo "Remote listening port of this application:["$r_port"]"
-            SID_info=Get_soft_array_table_sid $soft_directory
-	        NAME=`echo $SID_info | awk '{split($0,arr,",");print arr[1]}'`
-	        SID=`echo $SID_info | awk '{split($0,arr,",");print arr[2]}'`
-	        MAIN="com.lyh.game."$NAME".start.ServerStart"
-            `nohup java -server -Xms1024m -Xmx1024m -Xmn200m -Djava.rmi.server.hostname=${r_host} \
-            -Dcom.sun.management.jmxremote.port=${r_port} -Dcom.sun.management.jmxremote.authenticate=false \
-            -Dcom.sun.management.jmxremote.ssl=false -Xss256k -Xnoclassgc -XX:+ExplicitGCInvokesConcurrent \
-            -XX:+AggressiveOpts -XX:+UseParNewGC -XX:ParallelGCThreads=8 -XX:+UseConcMarkSweepGC -XX:ParallelCMSThreads=8 \
-            -XX:+UseFastAccessorMethods -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection \
-            -XX:CMSFullGCsBeforeCompaction=0 -XX:+UseBiasedLocking -XX:CMSInitiatingOccupancyFraction=70 \
-            -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+PrintClassHistogram -XX:+PrintGCDetails -XX:+PrintGCTimeStamps \
-            -XX:+PrintTenuringDistribution -Xloggc:log/gc.log -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m \
-            -cp $Release_soft_directory/lib/*:$file_name $MAIN ${SID} $Release_soft_directory $Release_soft_directory \
-            -Dfile.encoding=UTF-8 >log.txt 2>&1 &`
+            if [ $soft_directory = "game-ip" || $soft_directory = "game-pay" || \
+            $soft_directory = "game-promotion" || $soft_directory = "download-server" ];then
+                `nohup java -server -Xms1024m -Xmx1024m -Xmn200m -Djava.rmi.server.hostname="${r_host}" \
+                -Dcom.sun.management.jmxremote.port="${r_port}" -Dcom.sun.management.jmxremote.authenticate=false \
+                -Dcom.sun.management.jmxremote.ssl=false -Xss256k -Xnoclassgc -XX:+ExplicitGCInvokesConcurrent \
+                -XX:+AggressiveOpts -XX:+UseParNewGC -XX:ParallelGCThreads=8 -XX:+UseConcMarkSweepGC \
+                -XX:ParallelCMSThreads=8 -XX:+UseFastAccessorMethods -XX:+CMSParallelRemarkEnabled \
+                -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=0 -XX:+UseBiasedLocking \
+                -XX:CMSInitiatingOccupancyFraction=70 -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+PrintClassHistogram \
+                -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -Xloggc:log/gc.log \
+                -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m -jar "$file_name" -Dfile.encoding=UTF-8 >log.log 2>&1 &`
+            else
+                SID_info=Get_soft_array_table_sid $soft_directory
+	            NAME=`echo $SID_info | awk '{split($0,arr,",");print arr[1]}'`
+	            SID=`echo $SID_info | awk '{split($0,arr,",");print arr[2]}'`
+	            MAIN="com.lyh.game."$NAME".start.ServerStart"
+                `nohup java -server -Xms1024m -Xmx1024m -Xmn200m -Djava.rmi.server.hostname=${r_host} \
+                -Dcom.sun.management.jmxremote.port=${r_port} -Dcom.sun.management.jmxremote.authenticate=false \
+                -Dcom.sun.management.jmxremote.ssl=false -Xss256k -Xnoclassgc -XX:+ExplicitGCInvokesConcurrent \
+                -XX:+AggressiveOpts -XX:+UseParNewGC -XX:ParallelGCThreads=8 -XX:+UseConcMarkSweepGC -XX:ParallelCMSThreads=8 \
+                -XX:+UseFastAccessorMethods -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection \
+                -XX:CMSFullGCsBeforeCompaction=0 -XX:+UseBiasedLocking -XX:CMSInitiatingOccupancyFraction=70 \
+                -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+PrintClassHistogram -XX:+PrintGCDetails -XX:+PrintGCTimeStamps \
+                -XX:+PrintTenuringDistribution -Xloggc:log/gc.log -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m \
+                -cp $Release_soft_directory/lib/*:$file_name $MAIN ${SID} $Release_soft_directory $Release_soft_directory \
+                -Dfile.encoding=UTF-8 >log.log 2>&1 &`
+            fi
         else
             case $soft_directory in
                 $admin)
                 cd $Release_directory$soft_directory/bin/
                 ./startup.sh
                 ;;
-                apk)
+                $apk)
                 cd $Release_directory"apk_parser/dist/" 
                 ./apk.sh
                 ;;

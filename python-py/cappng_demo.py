8ec39d8d696f0a362a32eac3e598dd87437c5d74
3245554@qq.com <3245554@qq.com>
2019-04-13 18:45:09 +0800

    添加日子切割脚本

--- python-py/cappng_demo.py
+++ python-py/cappng_demo.py
@@ -10,7 +10,18 @@
  * Time: 下午6:25
 '''
 
-import pcap,time,dpkt,binascii
+import pcap,time,dpkt,requests,re,threading,socket
+from apscheduler import job
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, packes):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.packes = packes
+    def run(self):
+        jobs=job.Job()
+
 
 
 def mac_addr(mac):
@@ -44,9 +55,46 @@ def captcap():
             _win=TCP_data.win
             _data=TCP_data.data
             result_data =_data.decode("utf8","ignore")
-            print("%s SRC_IP: %s<SRC_MAC:%s>:%s-->DST_IP: %s<DST_MAC:%s>:%s [seq:%s ack:%s win:%s] data:%s"%(date,_src_ip,_src_mac,_src_port,_dst_ip,_dst_mac,_dst_port,_seq,_ack,_win,result_data))
+            noepacke=("%s SRC_IP: %s<SRC_MAC:%s>:%s-->DST_IP: %s<DST_MAC:%s>:%s [seq:%s ack:%s win:%s] data:%s"%(date,_src_ip,_src_mac,_src_port,_dst_ip,_dst_mac,_dst_port,_seq,_ack,_win,result_data))
+            print(noepacke)
+            wrapup({"date":date,"src_ip":_src_ip,"src_port":_src_port,"dst_ip":_dst_ip,"dst_port":_dst_port})
     else:
         print("None")
 
+def get_ip_by_ip138():
+    response = requests.get("http://2017.ip138.com/ic.asp")
+    ip = re.search(r"\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\]",response.content.decode(errors='ignore')).group(0)
+    return ip
+def get_lan_ip():
+    return socket.gethostbyname(socket.gethostname())
+
+def gethosttype():
+    pass
+
+def cloudhost():
+    pass
+
+def Physicshost():
+    pass
+
+
+def wrapup(kargs):
+    myintnetip=get_ip_by_ip138()
+    mylanip=get_lan_ip()
+    packge=[]
+    for pack in kargs:
+        if pack["src_ip"] != myintnetip and pack["src_ip"] !=mylanip:
+            packge.append(pack)
+
+def cuntpack():
+    timeunit=1
+    unitpacke=300
+    prohibitionunit=12
+
+
+
+
+
 if __name__ == '__main__':
+# 单ip每秒超过300包ip封禁12小时自动解
     captcap()

